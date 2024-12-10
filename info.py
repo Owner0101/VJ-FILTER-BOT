0b407f84caa7a0c3727ac29f25bc3cec327a26f1
Owner0101 <meet.extra.01@gmail.com>
2024-12-10 10:42:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,10 +112,10 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://instantearn.in/')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '0ce77ce31b9c932f836b2b13d8277a059378456b')
-VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/A1_Backup/86')
+VERIFY = bool(environ.get('VERIFY', False))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://instantearn.in/')
+SHORTLINK_API = environ.get('SHORTLINK_API', '0ce77ce31b9c932f836b2b13d8277a059378456b')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/A1_Backup/86') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
